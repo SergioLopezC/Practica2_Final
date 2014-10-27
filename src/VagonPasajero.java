2e2684d5c409eeae4091ce73ce1a699300e22c0c
SergioLopezC <sergio.lopez_j24@hotmail.com>
2014-10-27 16:54:43 -0600

    metodos clase VagonPasajero

--- src/VagonPasajero.java
+++ src/VagonPasajero.java
@@ -1,5 +1,19 @@
 
 public class VagonPasajero  {
-	
+	int npasajeros;
+	int nvagones;
+	public int getNpasajeros() {
+		return npasajeros;
+	}
+	public void setNpasajeros(int npasajeros) {
+		this.npasajeros = npasajeros;
+	}
+	public int getNvagones() {
+		return nvagones;
+	}
+	public void setNvagones(int nvagones) {
+		this.nvagones = nvagones;
+	}
+		
 	
 }
