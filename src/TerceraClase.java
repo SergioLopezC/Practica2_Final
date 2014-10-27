7448c0ba4cbdde55ad439d706dc3dcc6160c8de3
SergioLopezC <sergio.lopez_j24@hotmail.com>
2014-10-27 16:53:15 -0600

    metodos clase TerceraClase

--- src/TerceraClase.java
+++ src/TerceraClase.java
@@ -1,3 +1,25 @@
 public class TerceraClase {
-	
+int npasajeros;
+	int nvagones;
+	public int getNpasajeros() {
+		return npasajeros;
+	}
+	public void setNpasajeros(int npasajeros) {
+		this.npasajeros = npasajeros;
+	}
+	public int getNvagones() {
+		return nvagones;
+	}
+	public void setNvagones(int nvagones) {
+		this.nvagones = nvagones;
+	}
+	public void CalcularVagones(int suben,int bajan){
+		npasajeros = npasajeros+suben-bajan; 
+		int ayuda = npasajeros%40; 
+		if (ayuda==0) { 
+				  nvagones=npasajeros/40; 
+				}else{ 
+				  nvagones=(npasajeros+(40-ayuda))/40;
+				  }   
+	}	
 }
