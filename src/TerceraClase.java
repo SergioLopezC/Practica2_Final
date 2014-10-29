14069d0ddcb4d274d9853dd25dffd653fac7ce31
miltonXD <javierxd75@hotmail.com>
2014-10-29 04:42:57 +0100

    Update TerceraClase.java

--- src/TerceraClase.java
+++ src/TerceraClase.java
@@ -1,33 +1,5 @@
-public class TerceraClase {
-int npasajeros;
-	int nvagones;
-	int suben;
-	int bajan;
+public class TerceraClase extends VagonPasajero {
 	
-	public int getSuben() {
-		return suben;
-	}
-	public void setSuben(int suben) {
-		this.suben = suben;
-	}
-	public int getBajan() {
-		return bajan;
-	}
-	public void setBajan(int bajan) {
-		this.bajan = bajan;
-	}
-	public int getNpasajeros() {
-		return npasajeros;
-	}
-	public void setNpasajeros(int npasajeros) {
-		this.npasajeros = npasajeros;
-	}
-	public int getNvagones() {
-		return nvagones;
-	}
-	public void setNvagones(int nvagones) {
-		this.nvagones = nvagones;
-	}
 	public void CalcularVagones(int suben,int bajan){
 		npasajeros = npasajeros+suben-bajan; 
 		int ayuda = npasajeros%40; 
@@ -37,4 +9,5 @@ int npasajeros;
 				  nvagones=(npasajeros+(40-ayuda))/40;
 				  }   
 	}
+
 }
