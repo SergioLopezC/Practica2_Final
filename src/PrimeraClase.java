643281239eb3646c1dfe42369cba70e84d49ac8f
miltonXD <javierxd75@hotmail.com>
2014-10-29 04:41:59 +0100

    Update PrimeraClase.java

--- src/PrimeraClase.java
+++ src/PrimeraClase.java
@@ -1,35 +1,5 @@
-public class PrimeraClase  {
+public class PrimeraClase extends VagonPasajero{
 
-int npasajeros;
-		int nvagones=0;
-		int suben;
-		int bajan;
-		
-		public int getSuben() {
-			return suben;
-		}
-		public void setSuben(int suben) {
-			this.suben = suben;
-		}
-		public int getBajan() {
-			return bajan;
-		}
-		public void setBajan(int bajan) {
-			this.bajan = bajan;
-		}
-		public int getNpasajeros() {
-			return npasajeros;
-		}
-		public void setNpasajeros(int npasajeros) {
-			this.npasajeros = npasajeros;
-		}
-		public int getNvagones() { 
-			return nvagones;
-		}
-		public void setNvagones(int nvagones) {
-			this.nvagones = nvagones;
-		}
-		
 	public void CalcularVagones(int suben,int bajan){
 		npasajeros = npasajeros+suben-bajan; 
 		int ayuda = npasajeros%10; 
@@ -38,6 +8,7 @@ int npasajeros;
 				}else{ 
 				  nvagones=(npasajeros+(10-ayuda))/10;
 				  }   
-	}	
+	}
+
 
 }
